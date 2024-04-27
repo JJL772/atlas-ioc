e3ed0f3ca68fd798fe322566014a73df892e58d3
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-04-26 19:31:32 -0700

    Giant DBs for testing

---
+++ atlasApp/Db/gen-records.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser()  
+parser.add_argument('-o', type=str, required=True)
+parser.add_argument('-n', type=int, required=True)
+args = parser.parse_args()
+
+templ = '''
+record(longin, "$(P)Random$$NUM$$")
+{
+        field(VAL, "2")
+        field(DTYP, "devLongRandom")
+        field(SCAN, "1 second")
+        field(INP, "@$$HIGH$$,$$LOW$$")
+}
+'''
+
+with open(args.o, 'w') as fp:
+    for i in range(0, args.n):
+        fp.write(templ.replace('$$NUM$$', str(i)).replace('$$HIGH$$', '100').replace('$$LOW$$', '-100'))
+
+with open('auto_positions.req', 'w') as fp:
+    for i in range(0, args.n):
+        fp.write(f'$(P)Random{i}\n')
+
