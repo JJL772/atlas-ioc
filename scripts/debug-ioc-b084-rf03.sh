e8281e89c5d0bf03491a693d674894288908f902
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-10-22 16:27:05 -0700

    Update debug script

--- scripts/debug-ioc-b084-rf03.sh
+++ scripts/debug-ioc-b084-rf03.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
-powerpc-rtems-gdb -x 'target rtems-remote ioc-b084-rf03:2159'
+TOP="$(dirname "${BASH_SOURCE[0]}")"
 
+export PATH="/afs/slac/package/rtems/4.10.2/target/rtems_p4/ssrlApps_p3/powerpc-rtems/mvme3100/bin:$TOP/../bin/RTEMS-mvme3100:$PATH"
+
+echo "Running gdb..."
+powerpc-rtems-gdb -ex 'target rtems-remote ioc-b084-rf03:2159'
